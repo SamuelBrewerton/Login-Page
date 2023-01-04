cc2dfc32a78a5a1856d4ae6aca56b456f609bd7a
Samuel Brewerton <82506609+SamuelBrewerton@users.noreply.github.com>
2023-01-04 23:49:04 +1100

    Commit

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function login() {
     alert("Logged In");
-    bugattiColour = prompt("What colours your bugatti?");
-    open(""); //Insert redirect link. 
-    alert(bugattiColour);
+    open(""); 
 }
+
 
 
 
