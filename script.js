0267f0300638e559a68c7a33ac4e0f79affece63
Samuel Brewerton <82506609+SamuelBrewerton@users.noreply.github.com>
2023-01-23 22:53:03 +1100

    Login Validation

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-function login() {
+/*function login() {
     alert("Logged In");
-    open(""); 
+    open("");
 }
+*/
 
+function validateForm() {
+    // Get input elements
+    var login = document.forms["loginForm"]["username"];
+    var password = document.forms["loginForm"]["password"];
+
+    // Validate username field
+    if (username.value == "") {
+        alert("Please enter a valid username.");
+        username.focus();
+        return false;
+    }
+
+    // Validate password field
+    if (password.value == "") {
+        alert("Please enter a valid password.");
+        password.focus();
+        return false;
+    }
+
+    // If form is valid, submit ther form.
+    return true;
+}
 
 
 
